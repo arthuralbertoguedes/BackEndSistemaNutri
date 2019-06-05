e0233894f8789d7b071ef39ac88ee6716236e598
arthuralbertoguedes <arthuralberto_500@hotmail.com>
2019-06-04 22:05:35 -0300

    primeiro commit

---
+++ src/main/java/br/com/projetoREST/ProjetoRestApplication.java
@@ -0,0 +1,14 @@
+package br.com.projetoREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoRestApplication.class, args);
+	}
+
+}
+
